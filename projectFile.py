d947e009cc5cba85949bb9d8ce634cee3a74db8c
aditya-1407 <142811912+aditya-1407@users.noreply.github.com>
2023-09-05 19:24:29 +0530

    Update projectFile.py

--- projectFile.py
+++ projectFile.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt #Optional
 import itertools
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
